e4c914694fcd5f5bd98ace6b7a83cfe4edcc2a0f
AK-BuNNy <ananthakrishnan3644@gmail.com>
2025-06-23 19:01:17 +0530

    feat: consulation session log management, new web socket

--- src/consultation-sessions/consultation-sessions.controller.ts
+++ src/consultation-sessions/consultation-sessions.controller.ts
@@ -15,32 +15,45 @@ import {
   StartConsultationDto,
 } from './dto/create-consultation-session.dto';
 import { UpdateConsultationSessionDto } from './dto/update-consultation-session.dto';
+import { ResponseService } from 'src/response/response.service';
 
 @Controller('consultation-sessions')
 export class ConsultationSessionsController {
   constructor(
     private readonly consultationSessionsService: ConsultationSessionsService,
+    private readonly responseService: ResponseService,
   ) {}
 
   @Post('start')
-  start(@Body() dto: StartConsultationDto) {
-    return this.consultationSessionsService.startSession(
+  async start(@Body() dto: StartConsultationDto) {
+    const session = await this.consultationSessionsService.startSession(
       dto.bookingId,
       dto.createdBy,
+    );
+    return this.responseService.successResponse(
+      'session created Sucessfully',
+      session,
     );
   }
 
   @Post('connect')
-  connect(@Body() dto: ConnectConsultationDto) {
-    return this.consultationSessionsService.connectSession(dto.bookingId);
+  async connect(@Body() dto: ConnectConsultationDto) {
+    const response = await this.consultationSessionsService.connectSession(
+      dto.bookingId,
+    );
+    return this.responseService.successResponse(
+      'sucessfully connected',
+      response,
+    );
   }
 
   @Post('end')
-  end(@Body() dto: EndConsultationDto) {
-    return this.consultationSessionsService.endSession(
+  async end(@Body() dto: EndConsultationDto) {
+    const resposne = await this.consultationSessionsService.endSession(
       dto.bookingId,
       dto.endedBy,
       dto.notes,
     );
+    return this.responseService.successResponse('sucessfully ended', resposne);
   }
 }
--- src/consultation-sessions/consultation-sessions.module.ts
+++ src/consultation-sessions/consultation-sessions.module.ts
@@ -3,9 +3,10 @@ import { ConsultationSessionsService } from './consultation-sessions.service';
 import { ConsultationSessionsController } from './consultation-sessions.controller';
 import { ChatGateway } from 'src/chat/chat.gateway';
 import { ChatModule } from 'src/chat/chat.module';
+import { ResponseModule } from 'src/response/response.module';
 
 @Module({
-  imports: [ChatModule],
+  imports: [ChatModule, ResponseModule],
   controllers: [ConsultationSessionsController],
   providers: [ConsultationSessionsService],
 })
