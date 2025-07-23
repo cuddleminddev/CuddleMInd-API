a5bf0fb429aa39329a659917acc22e08fba03f2a
AK-BuNNy <ananthakrishnan3644@gmail.com>
2025-07-23 12:53:34 +0530

    feat: chat api

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,13 +1,16 @@
 // chat.controller.ts
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, Request, UseGuards } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('chat')
+@UseGuards(JwtAuthGuard)
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
   @Get('messages-by-sender')
-  async getMessagesBySender(@Query('senderId') senderId: string) {
+  async getMessagesBySender(@Request() req) {
+    const senderId = req.user.id;
     return this.chatService.getMessagesBySender(senderId);
   }
 }
